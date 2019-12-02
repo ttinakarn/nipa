433d15a5e36193b2e42d5017dcd411cbb783a6ae
ttinakarn <haiiplara@hotmail.com>
2019-12-02 15:59:04 +0700

    export as excel

--- src/views/VitalSignGraph.vue
+++ src/views/VitalSignGraph.vue
@@ -95,7 +95,7 @@ import moment from "moment";
 import chart from "@/components/Chart.vue";
 import VSTable from "@/components/VSTable.vue";
 import navbar from "@/components/NavbarHome.vue";
-import XLSX from 'xlsx';
+import XLSX from "xlsx";
 
 export default {
   components: {
@@ -107,6 +107,7 @@ export default {
     return {
       bedinfo: null,
       vitalsigns: [],
+      VS: null,
       showdata: false,
       temp: [],
       tempdate: [],
@@ -132,10 +133,8 @@ export default {
         console.log("vital sign data: ", response.data.data);
         instance.bedinfo = response.data.data[0];
         instance.vitalsigns = response.data.data;
+
         for (var i = 0; i < response.data.data.length; i++) {
-          instance.vitalsigns[i].admitdate = moment(instance.vitalsigns[i].admitdate).format('ll')
-          instance.vitalsigns[i].dischargedate = moment(instance.vitalsigns[i].dischargedate).format('ll')
-          instance.vitalsigns[i].date = moment(instance.vitalsigns[i].date).format('lll')
           if (response.data.data[i].temp != null) {
             instance.temp.push(response.data.data[i].temp);
             instance.tempdate.push(
@@ -143,8 +142,14 @@ export default {
             );
           }
         }
-        instance.lastesttemp = instance.temp.slice(instance.temp.length-8, instance.temp.length)
-        instance.lastesttempdate = instance.tempdate.slice(instance.tempdate.length-8, instance.tempdate.length)
+        instance.lastesttemp = instance.temp.slice(
+          instance.temp.length - 8,
+          instance.temp.length
+        );
+        instance.lastesttempdate = instance.tempdate.slice(
+          instance.tempdate.length - 8,
+          instance.tempdate.length
+        );
         for (var i = 0; i < response.data.data.length; i++) {
           if (response.data.data[i].pulse != null) {
             instance.pulse.push(response.data.data[i].pulse);
@@ -153,8 +158,14 @@ export default {
             );
           }
         }
-        instance.lastestpulse = instance.pulse.slice(instance.pulse.length-8, instance.pulse.length)
-        instance.lastestpulsedate = instance.pulsedate.slice(instance.pulsedate.length-8, instance.pulsedate.length)
+        instance.lastestpulse = instance.pulse.slice(
+          instance.pulse.length - 8,
+          instance.pulse.length
+        );
+        instance.lastestpulsedate = instance.pulsedate.slice(
+          instance.pulsedate.length - 8,
+          instance.pulsedate.length
+        );
       });
   },
   methods: {
@@ -162,10 +173,22 @@ export default {
       window.print();
     },
     onExport() {
-      const dataWS = XLSX.utils.json_to_sheet(this.vitalsigns)
-      const wb = XLSX.utils.book_new()
-      XLSX.utils.book_append_sheet(wb, dataWS)
-      XLSX.writeFile(wb, 'AN_'+this.$route.params.an + '.xlsx')
+      this.VS = JSON.parse(JSON.stringify(this.vitalsigns))
+      for (var i = 0; i < this.VS.length; i++) {
+        this.VS[i].admitdate = moment(
+          this.VS[i].admitdate
+        ).format("ll");
+        if (this.VS[i].dischargedate != null) {
+          this.VS[i].dischargedate = moment(
+            this.VS[i].dischargedate
+          ).format("ll");
+        }
+        this.VS[i].date = moment(this.VS[i].date).format("lll");
+      }
+      const dataWS = XLSX.utils.json_to_sheet(this.VS);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, dataWS);
+      XLSX.writeFile(wb, "AN_" + this.$route.params.an + ".xlsx");   
     }
   }
 };
