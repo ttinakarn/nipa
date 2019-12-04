926dafc75939ec398336af9c6400978028c54acc
ttinakarn <haiiplara@hotmail.com>
2019-12-04 08:55:53 +0700

    update dashboard

--- src/views/TV.vue
+++ src/views/TV.vue
@@ -57,6 +57,7 @@ export default {
       updatecol: [],
       updatecoldata: [],
       addnewrow: false,
+      cleareddata: false
     };
   },
   methods: {
@@ -104,7 +105,7 @@ export default {
                 }
               }
             }
-            if (i == instance.summary.length) {
+            if (i == instance.summary.length - 1) {
               instance.getsummary = true;
             }
           }
@@ -128,7 +129,7 @@ export default {
                 instance.summary[i].temp >= instance.sos[3].min &&
                 instance.summary[i].temp <= instance.sos[3].max
               ) {
-                instance.summary[i].sos += instance.sos[2].score;
+                instance.summary[i].sos += instance.sos[3].score;
               } else if (instance.summary[i].temp >= instance.sos[4].min) {
                 instance.summary[i].sos += instance.sos[4].score;
               }
@@ -241,7 +242,6 @@ export default {
   mounted() {
     var instance = this;
     this.getData();
-
     this.$store.commit("setgetdata", true);
     this.socket.on("dataUpdated", data => {
       console.log(data);
@@ -250,47 +250,160 @@ export default {
 
       for (var i in data.status) {
         this.$store.commit(
-          "set" + Object.keys(data.status[i])[0],
-          data.status[i][Object.keys(data.status[i])[0]]
-        );
-        if (data.status[i][Object.keys(data.status[i])[0]] == true) {
-          this.updatecol.push(Object.keys(data.status[i])[0]);
-          this.updatecoldata.push(data.status[i].data);
+          "set" + Object.keys(data.status[i])[0],false);
+        if(i < data.status.length){
+          this.cleareddata = true;
         }
+      }
+
+      if (this.cleareddata == true) {
+        for (var i in data.status) {
+          this.$store.commit(
+            "set" + Object.keys(data.status[i])[0],
+            data.status[i][Object.keys(data.status[i])[0]]
+          );
+          if (data.status[i][Object.keys(data.status[i])[0]] == true) {
+            this.updatecol.push(Object.keys(data.status[i])[0]);
+            this.updatecoldata.push(data.status[i].data);
+          }
+        }
+        this.cleareddata = false;
       }
 
       for (var i = 0; i < instance.summary.length; i++) {
         if (instance.summary[i].bednumber == data.bednumber) {
-          for (let j = 0; j < this.updatecol.length; j++) {
+          for (var j = 0; j < this.updatecol.length; j++) {
+            console.log(
+              "updated data",
+              instance.summary[i][this.updatecol[j]],
+              this.updatecoldata[j]
+            );
             instance.summary[i][this.updatecol[j]] = this.updatecoldata[j];
-            i = instance.summary.length-1;
+            console.log(
+              "updated data",
+              instance.summary[i][this.updatecol[j]],
+              this.updatecoldata[j]
+            );
+
+            //Calculate new sos score
+            instance.summary[i].sos = 0;
+            if (instance.summary[i].temp <= instance.sos[0].max) {
+              instance.summary[i].sos += instance.sos[0].score;
+            } else if (
+              instance.summary[i].temp >= instance.sos[1].min &&
+              instance.summary[i].temp <= instance.sos[1].max
+            ) {
+              instance.summary[i].sos += instance.sos[1].score;
+            } else if (
+              instance.summary[i].temp >= instance.sos[2].min &&
+              instance.summary[i].temp <= instance.sos[2].max
+            ) {
+              instance.summary[i].sos += instance.sos[2].score;
+            } else if (
+              instance.summary[i].temp >= instance.sos[3].min &&
+              instance.summary[i].temp <= instance.sos[3].max
+            ) {
+              instance.summary[i].sos += instance.sos[3].score;
+            } else if (instance.summary[i].temp >= instance.sos[4].min) {
+              instance.summary[i].sos += instance.sos[4].score;
+            }
+
+            if (instance.summary[i].pulse <= parseInt(instance.sos[5].max)) {
+              instance.summary[i].sos += instance.sos[5].score;
+            } else if (
+              instance.summary[i].pulse >= parseInt(instance.sos[6].min) &&
+              instance.summary[i].pulse <= parseInt(instance.sos[6].max)
+            ) {
+              instance.summary[i].sos += instance.sos[6].score;
+            } else if (
+              instance.summary[i].pulse >= parseInt(instance.sos[7].min) &&
+              instance.summary[i].pulse <= parseInt(instance.sos[7].max)
+            ) {
+              instance.summary[i].sos += instance.sos[7].score;
+            } else if (
+              instance.summary[i].pulse >= parseInt(instance.sos[8].min) &&
+              instance.summary[i].pulse <= parseInt(instance.sos[8].max)
+            ) {
+              instance.summary[i].sos += instance.sos[8].score;
+            } else if (
+              instance.summary[i].pulse >= parseInt(instance.sos[9].min) &&
+              instance.summary[i].pulse <= parseInt(instance.sos[9].max)
+            ) {
+              instance.summary[i].sos += instance.sos[9].score;
+            } else if (
+              instance.summary[i].pulse >= parseInt(instance.sos[10].min)
+            ) {
+              instance.summary[i].sos += instance.sos[10].score;
+            }
+
+            if (instance.summary[i].resp <= parseInt(instance.sos[17].max)) {
+              instance.summary[i].sos += instance.sos[17].score;
+            } else if (
+              instance.summary[i].resp >= parseInt(instance.sos[18].min) &&
+              instance.summary[i].resp <= parseInt(instance.sos[18].max)
+            ) {
+              instance.summary[i].sos += instance.sos[18].score;
+            } else if (
+              instance.summary[i].resp >= parseInt(instance.sos[19].min) &&
+              instance.summary[i].resp <= parseInt(instance.sos[19].max)
+            ) {
+              instance.summary[i].sos += instance.sos[19].score;
+            } else if (
+              instance.summary[i].resp >= parseInt(instance.sos[20].min) &&
+              instance.summary[i].resp <= parseInt(instance.sos[20].max)
+            ) {
+              instance.summary[i].sos += instance.sos[20].score;
+            } else if (
+              instance.summary[i].resp >= parseInt(instance.sos[21].min)
+            ) {
+              instance.summary[i].sos += instance.sos[21].score;
+            }
+
+            if (instance.summary[i].sbp <= parseInt(instance.sos[11].max)) {
+              instance.summary[i].sos += instance.sos[11].score;
+            } else if (
+              instance.summary[i].sbp >= parseInt(instance.sos[12].min) &&
+              instance.summary[i].sbp <= parseInt(instance.sos[12].max)
+            ) {
+              instance.summary[i].sos += instance.sos[12].score;
+            } else if (
+              instance.summary[i].sbp >= parseInt(instance.sos[13].min) &&
+              instance.summary[i].sbp <= parseInt(instance.sos[13].max)
+            ) {
+              instance.summary[i].sos += instance.sos[13].score;
+            } else if (
+              instance.summary[i].sbp >= parseInt(instance.sos[14].min) &&
+              instance.summary[i].sbp <= parseInt(instance.sos[14].max)
+            ) {
+              instance.summary[i].sos += instance.sos[14].score;
+            } else if (
+              instance.summary[i].sbp >= parseInt(instance.sos[15].min) &&
+              instance.summary[i].sbp <= parseInt(instance.sos[15].max)
+            ) {
+              instance.summary[i].sos += instance.sos[15].score;
+            } else if (
+              instance.summary[i].sbp >= parseInt(instance.sos[16].min)
+            ) {
+              instance.summary[i].sos += instance.sos[16].score;
+            }
           }
+          i = instance.summary.length - 1;
           instance.addnewrow = false;
         } else {
           instance.addnewrow = true;
         }
-        console.log(instance.addnewrow);
-        console.log(i);
-        
-        if(i == instance.summary.length-1 && instance.addnewrow == true){
+
+        if (i == instance.summary.length - 1 && instance.addnewrow == true) {
           console.log(i);
           console.log("Refresh page");
-          
+
           this.summary = [];
           this.getData();
           this.$store.commit("setgetdata", true);
           instance.addnewrow = false;
         }
       }
-      
-      // for (var i = 0; i < instance.summary.length; i++) {
-      //   if (instance.summary[i].bednumber != data.bednumber) {
-      //     this.summary = [];
-      //     this.getData();
-      //     this.$store.commit("setgetdata", true);
-      //   }
-      // }
-      
+
       this.updatecol = [];
       this.updatecoldata = [];
     });
